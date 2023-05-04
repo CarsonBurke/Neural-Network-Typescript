2323f790b9a783ea8629e00fa115670ff2553deb
Carson <carsonburke22@gmail.com>
2023-05-04 11:08:35 -0700

    improved js organization

--- public/neuralNetwork/networkUtils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function relu(value) {
-    return Math.max(0, value);
-}
-function mutateDelta() {
-    return (Math.random() * networkManager.learningRate - Math.random() * networkManager.learningRate);
-}
-//# sourceMappingURL=networkUtils.js.map
